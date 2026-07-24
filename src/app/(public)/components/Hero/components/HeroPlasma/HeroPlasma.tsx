ffb61682c49b15a841bb0f2e77a74baeaa3628b7
Thai Thanh Quan <thaithanhquan11102005@gmail.com>
2026-07-24 21:29:34 +0700

    update image from hero plasma

--- src/app/(public)/components/Hero/components/HeroPlasma/HeroPlasma.tsx
+++ src/app/(public)/components/Hero/components/HeroPlasma/HeroPlasma.tsx
@@ -1,18 +1,18 @@
-"use client";
+'use client';
 
-import { images } from "@/assets/images";
-import Lanyard from "@/components/Lanyard";
+import { images } from '@/assets/images';
+import Lanyard from '@/components/Lanyard';
 
 export default function HeroPlasma() {
   return (
-    <div className="pointer-events-none absolute -top-20 -right-4 h-90 w-[20%] lg:-top-25 lg:right-0 lg:left-auto lg:h-full lg:w-1/2">
+    <div className='pointer-events-none absolute -top-20 -right-4 h-90 w-[20%] lg:-top-25 lg:right-0 lg:left-auto lg:h-full lg:w-1/2'>
       <Lanyard
         position={[0, 0, 20]}
         gravity={[0, -40, 0]}
         frontImage={images.main}
         backImage={images.main}
-        imageFit="contain"
-        lanyardWidth={2}
+        imageFit='cover'
+        lanyardWidth={1.5}
       />
     </div>
   );
